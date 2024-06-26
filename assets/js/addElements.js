a17fdff6b212463606e1aa613998e19b8196e545
Kayque Othon <othoncode@gmail.com>
2024-06-26 08:43:36 -0300

    show json data in home screen

--- assets/js/addElements.js
+++ assets/js/addElements.js
@@ -5,5 +5,12 @@ var valor = document.querySelector('.valor');
 fetch('http://localhost:3333/data')
     .then(response => response.json())
     .then(data => {
-        console.log(data)
+        data.forEach(element => {
+            var elemento = document.createElement("div")
+            elemento.setAttribute('class', 'despesa')
+            var despesa = `${element.despesa}`
+            var valor = `${element.valor}`
+            elemento.innerHTML = `<h3>${despesa}</h3>` + `<h3>${valor}</h3>`;
+            despesas.appendChild(elemento)
+        });
     })
