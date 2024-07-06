d958e6003f41970ca5ff669ebc4cecc617f02f9a
Kayque Othon <othoncode@gmail.com>
2024-07-05 21:35:59 -0300

    show total prices sum

--- assets/js/addElements.js
+++ assets/js/addElements.js
@@ -2,18 +2,23 @@ var btn = document.querySelector('.btn');
 var despesas = document.querySelector('.despesas');
 var valor = document.querySelector('.valor');
 
-var idColetado;
+var totalValor = 0;
 
 fetch('http://localhost:3333/data')
     .then(response => response.json())
     .then(data => {
         data.forEach((element, index) => {
+
+            totalValor += parseFloat(element.valor)
+            valor.innerHTML = totalValor
+
             var elemento = document.createElement("div")
             elemento.setAttribute('class', 'despesa')
             elemento.setAttribute('id', `${index}`)
             var despesa = `${element.despesa}`
-            var valor = `${element.valor}`
-            elemento.innerHTML = `<h3>${despesa}</h3>` + `<h3>${valor}</h3>`;
+            var valor_despesa = parseFloat(`${element.valor}`)
+            elemento.innerHTML = `<h3>${despesa}</h3>` + `<h3>${valor_despesa}</h3>`;
+
             despesas.appendChild(elemento)
         });
     })
